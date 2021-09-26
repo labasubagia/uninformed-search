adcaa50f27e2d0eb27ec587ef57d2cb7fb0fd750
Laba Subagia <labasubagia22@gmail.com>
2021-09-26 20:32:06 +0800

    update graph test

--- graph_test.py
+++ graph_test.py
@@ -1,9 +1,15 @@
 import unittest
-from graph import dsf, bsf, get_first_node
+from graph import Graph, dsf, bsf, get_first_node
 from payload import dsf_graph, dsf_expected, bsf_graph, bsf_expected
 
 
 class TestGraphMethods(unittest.TestCase):
+
+    def test_get_first_node(self):
+        expected = '1'
+        graph: Graph = {f'{expected}': [], '2': []}
+        actual = get_first_node(graph)
+        self.assertEqual(actual, expected)
 
     def test_bsf(self):
         actual = bsf(bsf_graph, get_first_node(dsf_graph))
